f4c9e372a0a4715512caf7ab4fe4058afd89f4e4
KevTVKevin <xxkevtvxx@gmail.com>
2020-09-06 18:14:48 +0200

    Initial commit

---
+++ dungeons-paper/src/main/java/de/kevtvkevin/dungeons/Dungeons.java
@@ -0,0 +1,16 @@
+package de.kevtvkevin.dungeons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Dungeons extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
